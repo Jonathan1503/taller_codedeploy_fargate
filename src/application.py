283add8269c06b62a6d7d988ba0de55b1f9277da
Jonathan Rivera <j.riverao@uniandes.edu.co>
2025-05-05 18:05:07 -0500

    test

--- src/application.py
+++ src/application.py
@@ -6,6 +6,7 @@ data = f.load_file('./src/heroes.csv')
 
 @application.route("/")
 def index():
+    return jsonify({"response":"test"})
     return jsonify(data)
 
 @application.route("/<string:id>")
